f5524e0b8b68947189e4864cb77f4f9b2701fe92
Michel Jansen <mail@micheljansen.org>
2009-12-27 22:18:40 +0100

    added views

--- afcal.rb
+++ afcal.rb
@@ -4,31 +4,37 @@ require 'time'
 
 require 'lib/twente_milieu_data'
 
+# the full url where this app will be installed.
+BASE_URL = "http://localhost:9393"
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 get '/' do
-  "<ul>
-    #{all_events.map{|e| "<li>#{e.to_s}</li>"}.join("\n")}
-  </ul>"
+  erb :index
 end
 
 get '/:postalcode/:housenumber/all.:format' do
 
   #sanitize parameters
   raise "no postal code provided" if params[:postalcode].nil? 
-  postalcode = params[:postalcode].gsub(/[^a-z|^0-9]/i, "")[0..5]
+  @postalcode = params[:postalcode].gsub(/[^a-z|^0-9]/i, "")[0..5]
   
   raise "no house number provided" if params[:housenumber].nil?
-  housenumber = params[:housenumber].to_i
+  @housenumber = params[:housenumber].to_i
   
   time = Time.parse(params[:time]) if params[:time] && params[:time] != ""
   alarm = params[:alarm].to_i if params[:alarm]
   
   #fetch events
-  all_events = TwenteMilieuData.new(params[:postalcode], params[:housenumber], time).all_events
+  @all_events = TwenteMilieuData.new(params[:postalcode], params[:housenumber], time).all_events
   
   case params[:format]
   when "ics"
     content_type 'text/calendar'
-    icalendar = all_events.to_icalendar
+    icalendar = @all_events.to_icalendar
     
     if alarm
       icalendar.events.each do |event|
@@ -42,11 +48,6 @@ get '/:postalcode/:housenumber/all.:format' do
     
     icalendar.to_ical
   else
-    "
-    postcode: #{postalcode}<br/>
-    huisnummer: #{housenumber}<br />
-    <ul>
-      #{all_events.map{|e| "<li>#{e.to_s}</li>"}.join("\n")}
-    </ul>"
+    erb :calendar
   end
 end
